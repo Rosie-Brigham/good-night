42b4a7bf2573b5a9fcd9aedeea634a615610e283
Matt Weller <wellermatt@hotmail.com>
2014-11-08 16:41:49 +0000

    DB rev 2

--- db/create_db.sql
+++ db/create_db.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2014-11-08 14:33:14
+Date: 2014-11-08 16:41:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,11 +20,18 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `drink`;
 CREATE TABLE `drink` (
-`drink_id`  int(11) NOT NULL ,
+`drink_id`  int(11) NOT NULL AUTO_INCREMENT ,
+`drink_name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
+`size_ml`  double NULL DEFAULT NULL ,
+`abv`  double NULL DEFAULT NULL ,
+`alcohol_ml`  double NULL DEFAULT NULL ,
+`units`  double NULL DEFAULT NULL ,
+`calories`  double NULL DEFAULT NULL ,
 PRIMARY KEY (`drink_id`)
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=11
 
 ;
 
@@ -32,6 +39,7 @@ DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
 -- Records of drink
 -- ----------------------------
 BEGIN;
+INSERT INTO `drink` VALUES ('1', 'Wine - S', '125', '13', null, null, '128'), ('2', 'Wine - M', '175', '13', null, null, '179'), ('3', 'Wine - L', '250', '13', null, null, '255'), ('4', 'Beer (pint)', '568', '4.7', null, null, '239'), ('5', 'Beer (bottle)', '330', '4.7', null, null, '139'), ('6', 'Beer (half)', '284', '4.7', null, null, '120'), ('7', 'Spirit', '25', '40', null, null, '229'), ('8', 'Mixer - Fizzy', '100', '0', null, null, '138'), ('9', 'Mixer - Juice', '100', '0', null, null, '168'), ('10', 'Alcopop', '275', '5.5', null, null, '228');
 COMMIT;
 
 -- ----------------------------
@@ -39,7 +47,7 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `night`;
 CREATE TABLE `night` (
-`night_id`  int(11) NOT NULL ,
+`night_id`  int(11) NOT NULL AUTO_INCREMENT ,
 `user_id`  int(11) NULL DEFAULT NULL ,
 `start_datetime`  datetime NULL DEFAULT NULL ,
 `end_datetime`  datetime NULL DEFAULT NULL ,
@@ -51,6 +59,7 @@ INDEX `FK_night_user` (`user_id`) USING BTREE
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=1
 
 ;
 
@@ -65,18 +74,19 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `night_drink`;
 CREATE TABLE `night_drink` (
-`night_drink_id`  int(11) NOT NULL ,
+`night_drink_id`  int(11) NOT NULL AUTO_INCREMENT ,
 `night_id`  int(11) NULL DEFAULT NULL ,
 `drink_id`  int(11) NULL DEFAULT NULL ,
 `night_drink_time`  datetime NULL DEFAULT NULL ,
 PRIMARY KEY (`night_drink_id`),
 FOREIGN KEY (`drink_id`) REFERENCES `drink` (`drink_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
 FOREIGN KEY (`night_id`) REFERENCES `night` (`night_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-INDEX `FK_night_drink_drink` (`drink_id`) USING BTREE ,
-INDEX `FK_night_drink_night` (`night_id`) USING BTREE 
+INDEX `FK_night_drink_night` (`night_id`) USING BTREE ,
+INDEX `FK_night_drink_drink` (`drink_id`) USING BTREE 
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=1
 
 ;
 
@@ -91,7 +101,7 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-`user_id`  int(11) NOT NULL ,
+`user_id`  int(11) NOT NULL AUTO_INCREMENT ,
 `email`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
 `fb_id`  bigint(20) NULL DEFAULT NULL ,
 `gender`  varchar(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
@@ -102,6 +112,7 @@ PRIMARY KEY (`user_id`)
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+AUTO_INCREMENT=1
 
 ;
 
@@ -110,3 +121,23 @@ DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
 -- ----------------------------
 BEGIN;
 COMMIT;
+
+-- ----------------------------
+-- Auto increment value for drink
+-- ----------------------------
+ALTER TABLE `drink` AUTO_INCREMENT=11;
+
+-- ----------------------------
+-- Auto increment value for night
+-- ----------------------------
+ALTER TABLE `night` AUTO_INCREMENT=1;
+
+-- ----------------------------
+-- Auto increment value for night_drink
+-- ----------------------------
+ALTER TABLE `night_drink` AUTO_INCREMENT=1;
+
+-- ----------------------------
+-- Auto increment value for user
+-- ----------------------------
+ALTER TABLE `user` AUTO_INCREMENT=1;
