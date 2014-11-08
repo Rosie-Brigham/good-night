2fff2aec26076e712a6ffb7a85b3dcbd126f00d2
Matt Weller <wellermatt@hotmail.com>
2014-11-08 14:33:58 +0000

    DB rev 1

--- db/create_db.sql
+++ db/create_db.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2014-11-08 13:51:59
+Date: 2014-11-08 14:33:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -30,6 +30,58 @@ DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
 
 -- ----------------------------
 -- Records of drink
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for night
+-- ----------------------------
+DROP TABLE IF EXISTS `night`;
+CREATE TABLE `night` (
+`night_id`  int(11) NOT NULL ,
+`user_id`  int(11) NULL DEFAULT NULL ,
+`start_datetime`  datetime NULL DEFAULT NULL ,
+`end_datetime`  datetime NULL DEFAULT NULL ,
+`runtot_calories`  double(255,0) NULL DEFAULT NULL ,
+`runtot_units`  double(255,0) NULL DEFAULT NULL ,
+PRIMARY KEY (`night_id`),
+FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+INDEX `FK_night_user` (`user_id`) USING BTREE 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+
+;
+
+-- ----------------------------
+-- Records of night
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for night_drink
+-- ----------------------------
+DROP TABLE IF EXISTS `night_drink`;
+CREATE TABLE `night_drink` (
+`night_drink_id`  int(11) NOT NULL ,
+`night_id`  int(11) NULL DEFAULT NULL ,
+`drink_id`  int(11) NULL DEFAULT NULL ,
+`night_drink_time`  datetime NULL DEFAULT NULL ,
+PRIMARY KEY (`night_drink_id`),
+FOREIGN KEY (`drink_id`) REFERENCES `drink` (`drink_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+FOREIGN KEY (`night_id`) REFERENCES `night` (`night_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+INDEX `FK_night_drink_drink` (`drink_id`) USING BTREE ,
+INDEX `FK_night_drink_night` (`night_id`) USING BTREE 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
+
+;
+
+-- ----------------------------
+-- Records of night_drink
 -- ----------------------------
 BEGIN;
 COMMIT;
@@ -55,32 +107,6 @@ DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
 
 -- ----------------------------
 -- Records of user
--- ----------------------------
-BEGIN;
-COMMIT;
-
--- ----------------------------
--- Table structure for user_drink
--- ----------------------------
-DROP TABLE IF EXISTS `user_drink`;
-CREATE TABLE `user_drink` (
-`user_drink_id`  int(11) NOT NULL ,
-`user_id`  int(11) NULL DEFAULT NULL ,
-`drink_id`  int(11) NULL DEFAULT NULL ,
-`user_drink_time`  datetime NULL DEFAULT NULL ,
-PRIMARY KEY (`user_drink_id`),
-FOREIGN KEY (`drink_id`) REFERENCES `drink` (`drink_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-INDEX `FK_user_drink_user` (`user_id`) USING BTREE ,
-INDEX `FK_user_drink_drink` (`drink_id`) USING BTREE 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci
-
-;
-
--- ----------------------------
--- Records of user_drink
 -- ----------------------------
 BEGIN;
 COMMIT;
